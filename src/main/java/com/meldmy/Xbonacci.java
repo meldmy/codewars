878d875b8e3d364e188a3197bcce3c4bc3f076ce
Dmytro Melnychuk <melnichukdv@gmail.com>
2018-09-13 22:06:16 +0200

    Simplify tribonacci

--- src/main/java/com/meldmy/Xbonacci.java
+++ src/main/java/com/meldmy/Xbonacci.java
@@ -5,7 +5,6 @@ package com.meldmy;
  */
 public class Xbonacci {
     public double[] tribonacci(double[] s, int n) {
-        if (s.length == 0 || n == 0) return new double[0];
         double[] res = new double[n];
         for (int i = 0; i < n; i++) {
             res[i] = i < 3 ? s[i] : res[i - 3] + res[i - 2] + res[i - 1];
